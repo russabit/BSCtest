a6eb7e7ca425e38e38540884735d5a5c2aedf633
russabit <50712640+russabit@users.noreply.github.com>
2020-04-22 22:51:35 +0300

    removed random numbers - didn't work on my xiaomi a1

--- app/src/main/java/ru/ruslan/bsctest/utils/AccountsCreator.kt
+++ app/src/main/java/ru/ruslan/bsctest/utils/AccountsCreator.kt
@@ -1,24 +1,21 @@
 package ru.ruslan.bsctest.utils
 
 import ru.ruslan.bsctest.R
-import java.util.*
 import kotlin.collections.ArrayList
 
 class AccountsCreator {
     companion object {
         fun getAccountsList(): ArrayList<Account> {
             val accountsList = ArrayList<Account>()
-
-            //accountsList.add(Account("RUR", 124128, R.drawable.rur_image))
-            accountsList.add(Rur(balance = Random().nextDouble().times(100000).round()))
-            accountsList.add(Usd(balance = Random().nextDouble().times(10000).round()))
-            accountsList.add(Rur(balance = Random().nextDouble().times(100000).round()))
-            accountsList.add(Rur(balance = Random().nextDouble().times(100000).round()))
+            accountsList.add(Rur(balance = 37232.24))
+            accountsList.add(Usd(balance = 1242142.21))
+            accountsList.add(Rur(balance = 124231.32))
+            accountsList.add(Rur(balance = 74620.12))
             accountsList.add(Eur())
             accountsList.add(Eur())
-            accountsList.add(Usd(balance = Random().nextDouble().times(100000).round()))
-            accountsList.add(Rur(balance = Random().nextDouble().times(100000).round()))
-            accountsList.add(Usd(balance = Random().nextDouble().times(10000).round()))
+            accountsList.add(Usd(balance = 1243.64))
+            accountsList.add(Rur(balance = 25732.12))
+            accountsList.add(Usd(balance = 756.21))
             return accountsList
         }
     }
@@ -37,9 +34,4 @@ data class Usd(override val currencyName: String = "USD",
 
 data class Eur(override val currencyName: String = "EUR",
           val currencyPicture: Int = R.drawable.eur_image) : Account(currencyName)
-
-
-fun Double.round(decimals: Int = 2): Double =
-    "%.${decimals}f".format(this).toDouble()
-
 
