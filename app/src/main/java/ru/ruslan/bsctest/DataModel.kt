e7996601db3418ac9d1a74f06337cf1307f3021d
russabit <50712640+russabit@users.noreply.github.com>
2020-04-22 22:29:43 +0300

    done

--- app/src/main/java/ru/ruslan/bsctest/DataModel.kt
+++
@@ -1,8 +0,0 @@
-package ru.ruslan.bsctest
-
-import ru.ruslan.bsctest.utils.Account
-import ru.ruslan.bsctest.utils.AccountsCreator
-
-class DataModel {
-    val accountsList: ArrayList<Account> = AccountsCreator.getAccountsList()
-}
